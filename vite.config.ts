bdab50a9481fba73b57462a688db8cec4a010c7e
Thijmen Rierink <thijmen@rierink.dev>
2026-05-18 18:18:08 +0200

    feat: add robots.txt for search engine indexing and update Vite config to include favicon

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     svelte(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['opensearch.xml'],
+      includeAssets: ['opensearch.xml', 'favicon.svg', 'robots.txt'],
     }),
   ],
 })
