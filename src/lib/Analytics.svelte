07bd205a51031c4d577b8df523aeebe714917584
NietThijmen <53520119+NietThijmen@users.noreply.github.com>
2026-05-19 00:04:26 +0200

    feat(analytics): update Vercel analytics import and adjust event handling

--- src/lib/Analytics.svelte
+++ src/lib/Analytics.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
     import {onMount} from "svelte";
-    import { injectAnalytics } from '@vercel/analytics/sveltekit';
+    import { inject } from '@vercel/analytics';
 
     const isProd = import.meta.env.PROD;
 
     onMount(() => {
-        injectAnalytics({
+        inject({
             mode: isProd ? 'production' : 'development',
             beforeSend: event => {
                 const url = new URL(event.url);
