fa4594742f8f34172cdc0f3019db3102b344b980
NietThijmen <53520119+NietThijmen@users.noreply.github.com>
2026-05-19 00:00:18 +0200

    feat(analytics): add Vercel analytics integration and update dependencies

---
+++ src/lib/Analytics.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import {onMount} from "svelte";
+    import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+    const isProd = import.meta.env.PROD;
+
+    onMount(() => {
+        injectAnalytics({
+            mode: isProd ? 'production' : 'development',
+
+        })
+    });
+</script>
